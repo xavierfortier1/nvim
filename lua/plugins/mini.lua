0679b8f9e1c871ed2cdab801820c4e111de7b34f
Xavier Fortier <xavier.fortier@proton.me>
2025-02-14 09:29:04 -0500

    improved comments handling

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,5 +1,33 @@
 return {
   {
+    "echasnovski/mini.ai",
+    version = false,
+    opts = {},
+  },
+  {
+    "echasnovski/mini.comment",
+    version = false,
+    opts = {
+      options = {
+        ignore_blank_line = true,
+      },
+    },
+  },
+  {
+    "echasnovski/mini.diff",
+    version = false,
+    opts = {
+      view = {
+        style = "sign",
+        signs = {
+          add = "▎",
+          change = "▎",
+          delete = "",
+        },
+      },
+    },
+  },
+  {
     "echasnovski/mini.pairs",
     version = false,
     opts = {},
