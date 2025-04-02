28366048d83422e61822b844c9383b9ba5973349
Xavier Fortier <xavier.fortier@proton.me>
2025-04-02 15:22:30 -0400

    added virt lines on hover

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,25 +12,69 @@ return {
           end
 
           if client:supports_method("textDocument/hover") then
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+            local hover_opts = vim.tbl_extend("force", opts, { desc = "Hover" })
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, hover_opts)
           end
           if client:supports_method("textDocument/rename") then
-            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+            local rename_opts = vim.tbl_extend("force", opts, { desc = "Lsp Rename" })
+            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, rename_opts)
           end
           if client:supports_method("textDocument/codeAction") then
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+            local code_action_opts = vim.tbl_extend("force", opts, { desc = "Code Actions" })
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, code_action_opts)
           end
           if client:supports_method("textDocument/formatting") then
-            vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, opts)
+            local format_opts = vim.tbl_extend("force", opts, { desc = "Format Buffers" })
+            vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, format_opts)
 
             vim.api.nvim_create_autocmd("BufWritePre", {
-              buffer = args.buf,
               callback = function()
-                -- vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-                vim.lsp.buf.format(opts)
+                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+                -- vim.lsp.buf.format(opts)
               end,
             })
           end
+        end,
+      })
+
+      local virt_text, virt_lines
+      vim.api.nvim_create_autocmd({ "CursorMoved", "DiagnosticChanged" }, {
+        callback = function()
+          local buf = vim.api.nvim_get_current_buf()
+          local ft = { "lazy", "TelescopePrompt", "neo-tree" }
+          if vim.tbl_contains(ft, vim.bo[buf].filetype) then
+            return
+          end
+
+          if not virt_lines then
+            virt_lines = vim.diagnostic.config().virtual_lines
+          end
+
+          if not (virt_lines and virt_lines.current_line) then
+            if virt_text then
+              vim.diagnostic.config({ virtual_text = virt_text })
+              virt_text = nil
+            end
+            return
+          end
+
+          if not virt_text then
+            virt_text = vim.diagnostic.config().virtual_text
+          end
+
+          local lnum = vim.api.nvim_win_get_cursor(0)[1] - 1
+
+          if vim.tbl_isempty(vim.diagnostic.get(0, { lnum = lnum })) then
+            vim.diagnostic.config({ virtual_text = virt_text })
+          else
+            vim.diagnostic.config({ virtual_text = false })
+          end
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("ModeChanged", {
+        callback = function()
+          pcall(vim.diagnostic.show)
         end,
       })
 
@@ -44,12 +88,9 @@ return {
           prefix = "",
         },
         signs = {
-          text = {
-            [vim.diagnostic.severity.ERROR] = "",
-            [vim.diagnostic.severity.WARN] = "",
-            [vim.diagnostic.severity.INFO] = "",
-            [vim.diagnostic.severity.HINT] = "",
-          },
+          text = vim.tbl_map(function()
+            return ""
+          end, vim.diagnostic.severity),
           numhl = {
             [vim.diagnostic.severity.WARN] = "WarningMsg",
             [vim.diagnostic.severity.ERROR] = "ErrorMsg",
@@ -57,7 +98,9 @@ return {
             [vim.diagnostic.severity.HINT] = "DiagnosticHint",
           },
         },
-        virtual_lines = true,
+        virtual_text = true,
+        virtual_lines = { current_line = true },
+        severity_sort = true,
       })
     end,
   },
